a2791cc8e73d6145bc6aa516bf6a22e7a79547ec
xxw <18271698552@163.com>
2022-03-15 11:07:33 +0800

    模块依赖梳理

--- shanjupay/shanjupay-merchant/shanjupay-merchant-service/target/generated-sources/annotations/com/shanjupay/merchant/convert/MerchantConvertImpl.java
+++ shanjupay/shanjupay-merchant/shanjupay-merchant-service/target/generated-sources/annotations/com/shanjupay/merchant/convert/MerchantConvertImpl.java
@@ -8,8 +8,8 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-12-17T16:05:27+0800",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_152 (Oracle Corporation)"
+    date = "2022-03-15T11:05:57+0800",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_301 (Oracle Corporation)"
 )
 public class MerchantConvertImpl implements MerchantConvert {
 
--- shanjupay/shanjupay-transaction/shanjupay-transaction-service/target/generated-sources/annotations/com/shanjupay/transaction/convert/PlatformChannelConvertImpl.java
+++ shanjupay/shanjupay-transaction/shanjupay-transaction-service/target/generated-sources/annotations/com/shanjupay/transaction/convert/PlatformChannelConvertImpl.java
@@ -8,8 +8,8 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-01-17T11:02:22+0800",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_171 (Oracle Corporation)"
+    date = "2022-03-15T11:06:02+0800",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_301 (Oracle Corporation)"
 )
 public class PlatformChannelConvertImpl implements PlatformChannelConvert {
 
